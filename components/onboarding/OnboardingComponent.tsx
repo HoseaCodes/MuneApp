a5e3c177c18445342347f1fef9134893f74aeab8
Dominique Hosea <dominique11h@yahoo.com>
2024-09-22 14:11:44 -0500

    feat: Add progress bar to OnboardingComponent

--- components/onboarding/OnboardingComponent.tsx
+++ components/onboarding/OnboardingComponent.tsx
@@ -1,12 +1,35 @@
+import React, { useState } from 'react'
 import { View, Text, FlatList, Button } from 'react-native'
-import React from 'react'
 import { OnboardingCard } from './OnboardingCard'
+import ProgressBar from './Progressbar';
 
 export default function OnboardingComponent({navigation}: {navigation: any}) {
+    const [activeSection, setActiveSection] = useState(2);
+
+    const nextSection = () => {
+        setActiveSection((prev) => (prev < 5 ? prev + 1 : 5));
+    };
+
+    const backSection = () => {
+        setActiveSection((prev) => (prev > 2 ? prev - 1 : 2));
+    };
+
+
     return (
-        <View style={{display: 'flex', flexDirection: "column", justifyContent: "space-evenly"}}>
+        <View style={{
+            display: 'flex', 
+            flexDirection: "column", 
+            justifyContent: "space-evenly",
+            padding: 20,
+            alignItems: "center",
+            gap: 20
+            }}>
             <Text>Logo</Text>
-            <Text>progress bar</Text>
+            <ProgressBar activeSection={activeSection} />
+            <View style={{ marginVertical: 20 }}>
+                <Button title="Next Section" onPress={nextSection} />
+                <Button title="Back Section" onPress={backSection} />
+            </View>
             <FlatList
                 data={[{etag: '1', title: 'title', description: 'description'}, {etag: '2', title: 'title', description: 'description'}]}
                 keyExtractor={(item) => item.etag}
