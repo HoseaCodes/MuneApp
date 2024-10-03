2f77962dcd95272e76f702970c568a1057aaeb54
Dominique Hosea <dominique11h@yahoo.com>
2024-10-03 14:16:01 -0500

    updated configs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './' // This should match the alias in your tsconfig.json
+          }
+        }
+      ]
+    ]
   };
 };
