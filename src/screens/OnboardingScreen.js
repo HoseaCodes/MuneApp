1459b54963bab19fad65e5469185198dd08c2378
Dominique Hosea <dominique11h@yahoo.com>
2024-10-07 20:36:41 -0500

    Mune logo header

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,85 +1,17 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Dimensions } from "react-native";
-import HeaderProgressBar from '../../components/HeaderProgressBar';
+import { View, Text, Button } from "react-native";
+import OnboardingComponent from "../../components/onboarding/OnboardingComponent";
 
 const OnboardingScreen = ({ navigation }) => {
-  const { width } = Dimensions.get("window"); // Get screen width for responsive button width
-
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#ffffff", // Screen background color
-      }}
-    >
-
-      {/* Render the header progress bar */}
-      <HeaderProgressBar totalPages={4} currentPage={0} />
-
-      <Text
-        style={{
-          fontSize: 24,
-          fontWeight: "bold",
-          marginBottom: 20,
-          color: "#333333",
-        }}
-      >
-        Welcome to Mun-e!
-      </Text>
-
-      {/* Replacing Button with TouchableOpacity to apply custom styling */}
-      <TouchableOpacity
-        style={{
-          backgroundColor: "#19A530",
-          width: width * 0.78, // Responsive width based on screen width
-          height: 56,
-          borderRadius: 14,
-          justifyContent: "center", // Center text vertically
-          alignItems: "center", // Center text horizontally
-        }}
-        onPress={() => navigation.navigate("Register")}
-      >
-        <Text
-          style={{
-            fontFamily: "Poppins", 
-            fontSize: 16, 
-            fontWeight: "700", 
-            lineHeight: 24,
-            color: "#FFFFFF",
-            textAlign: "center",
-          }}
-        >
-          Sign Up
-        </Text>
-        
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          backgroundColor: "#FFFFFF",
-          width: width * 0.78, // Responsive width based on screen width
-          height: 56,
-          borderRadius: 14,
-          justifyContent: "center", // Center text vertically
-          alignItems: "center", // Center text horizontally
-        }}
-        onPress={() => navigation.navigate("Register")}
-      >
-        <Text
-          style={{
-            fontFamily: "Poppins", 
-            fontSize: 16, 
-            fontWeight: "700", 
-            lineHeight: 24,
-            color: "#19A530",
-            textAlign: "center",
-          }}
-        >
-          Log in
-        </Text>
-        
-      </TouchableOpacity>
+    <View>
+      <OnboardingComponent navigation={navigation}>
+        <Text>Welcome to Mun-e!</Text>
+        <Button
+          title="Get Started"
+          onPress={() => navigation.navigate("Register")}
+        />
+      </OnboardingComponent>
     </View>
   );
 };
