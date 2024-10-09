aff500d1539ee55a541ef852cb7a5c51fd874951
Riviere Fougy <rivierefougy@gmail.com>
2024-10-08 20:25:33 -0400

    refactor: QRCodeModal styling

--- src/components/profile/QRCodeModal.tsx
+++ src/components/profile/QRCodeModal.tsx
@@ -1,10 +1,22 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import { Modal, View, Text, Button, StyleSheet } from 'react-native';
+import React, { Dispatch, SetStateAction, useState } from "react";
+import {
+  Modal,
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import Header from "../Header";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
-const QRCodeModal: React.FC<{modalVisible: boolean; setModalVisible: Dispatch<SetStateAction<boolean>>}> = ({modalVisible, setModalVisible}) => {
+const QRCodeModal: React.FC<{
+  modalVisible: boolean;
+  setModalVisible: Dispatch<SetStateAction<boolean>>;
+}> = ({ modalVisible, setModalVisible }) => {
   return (
     <View style={styles.container}>
-      <Button title="Show Modal" onPress={() => setModalVisible(true)} />
       <Modal
         animationType="slide"
         transparent={true}
@@ -12,8 +24,22 @@ const QRCodeModal: React.FC<{modalVisible: boolean; setModalVisible: Dispatch<Se
         onRequestClose={() => setModalVisible(false)}
       >
         <View style={styles.modalView}>
-          <Text>This is a Modal</Text>
-          <Button title="Hide Modal" onPress={() => setModalVisible(false)} />
+          <Header
+            title="Mun-e ID"
+            IconType={AntDesign}
+            iconLeft="left"
+            iconRight="upload"
+          />
+          <Image
+            source={require("../../../assets/images/qrcode-placeholder.png")}
+            style={styles.imageContainer}
+          />
+          <TouchableOpacity
+            onPress={() => setModalVisible(false)}
+            style={styles.hideButton}
+          >
+            <Text style={styles.hideButtonText}>Hide Modal</Text>
+          </TouchableOpacity>
         </View>
       </Modal>
     </View>
@@ -22,21 +48,41 @@ const QRCodeModal: React.FC<{modalVisible: boolean; setModalVisible: Dispatch<Se
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
+    fontWeight: "bold",
   },
   modalView: {
-    marginTop: 'auto', // This will push it to the bottom
-    backgroundColor: 'white',
+    display: "flex",
+    flexDirection: "column",
+    gap: 50,
+    marginTop: "auto",
+    backgroundColor: "white",
     padding: 20,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOpacity: 0.3,
     shadowRadius: 10,
     elevation: 5,
   },
+  imageContainer: {
+    width: 345,
+    height: 345,
+    marginVertical: -40,
+    borderRadius: 34,
+    borderColor: "#CEE0D0",
+    borderWidth: 2,
+    shadowColor: "#1DBF38",
+    shadowOpacity: 0.7,
+    shadowRadius: 3,
+    shadowOffset: { width: 5, height: 5 },
+    elevation: 10,
+  },
+  hideButton: {},
+  hideButtonText: {},
 });
 
 export default QRCodeModal;
