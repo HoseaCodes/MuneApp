35780d27bbe90cf807ef541118da55eef1816ffd
Riviere Fougy <rivierefougy@gmail.com>
2024-10-10 19:17:53 -0400

    refactor(QRCode): ensure url can be visited, styling

--- src/components/profile/QRCode.tsx
+++ src/components/profile/QRCode.tsx
@@ -1,54 +1,55 @@
-import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
-import QRCodeStyled from 'react-native-qrcode-styled';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import QRCodeStyled from "react-native-qrcode-styled";
 
 const QRCode: React.FC = () => (
-    <View style={styles.root}>
-      <QRCodeStyled
-        data={'https://www.google.com/'}
-        style={styles.svg}
-        padding={20}
-        pieceSize={8}
-        pieceBorderRadius={4}
-        color={'#19A530'}
-        innerEyesOptions={{
-          borderRadius: 4.76,
-          color: '#19A530',
-        }}
-        outerEyesOptions={{
-          borderRadius: 15.22,
-          color: '#19A530',
-        }}
-      />
-      <View style={styles.logoContainer}>
-        <Image source={require('../../../assets/images/profile.png')} style={styles.logo} />
-      </View> 
-    </View>
-  );
+  <View style={styles.root}>
+    <QRCodeStyled
+      data={`https://www.mun-e.com/profile/johnny-boy`}
+      style={styles.svg}
+      padding={20}
+      pieceSize={8}
+      pieceBorderRadius={4}
+      color={"#19A530"}
+      innerEyesOptions={{
+        borderRadius: 4.76,
+        color: "#19A530",
+      }}
+      outerEyesOptions={{
+        borderRadius: 15.22,
+        color: "#19A530",
+      }}
+      logo={{
+        href: require("../../../assets/images/profile.png"),
+        padding: 6,
+        scale: 1.1,
+      }}
+    />
+  </View>
+);
 
 const styles = StyleSheet.create({
   root: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   svg: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 20,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   logoContainer: {
-    position: 'absolute',
-    width: 60,
-    height: 60,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: "absolute",
+    width: 50,
+    height: 50,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
   logo: {
-    width: '90%',
-    height: '90%',
-    top: -2,
-    borderRadius: 16
+    width: "90%",
+    height: "90%",
+    borderRadius: 12.5,
   },
 });
 
